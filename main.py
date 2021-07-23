e4d3a5321df085a762f26dbb6fa799cdf356238e
Alvan AlAmien <zigax201@gmail.com>
2021-07-23 15:07:28 +0700

    add new route

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, session
+from flask import Flask, redirect, url_for, session, jsonify
 from flask_cors import CORS
 from flask_restful import Api, Resource, reqparse
 from flask_mysqldb import MySQL
@@ -41,36 +41,36 @@ data_put_args.add_argument("id", type=int, help="id of the data", required=True)
 link_post_args = reqparse.RequestParser()
 link_post_args.add_argument("link", type=str, help="link of the data", required=True)
 
-class tableauData(Resource):
-    def get(self, id):
-        cur = mysql.connection.cursor()
-        cur.callproc('calculateAHP1', (id))
-        dataAhp = cur.fetchall()
-        data = []
-        for f in dataAhp:
-            dat = {
-                'name' : f[1],
-                'jumlahAtt1' : f[2],
-                'jumlahAtt2' : f[3],
-                'jumlahAtt3' : f[4],
-                'rataAtt1' : f[5],
-                'rataAtt2' : f[6],
-                'rataAtt3' : f[7],
-                'matrix1' : f[8],
-                'matrix2' : f[9],
-                'matrix3' : f[10]
-            }
-            data.append(dat)
-        jsonString = json.loads(json.dumps(data, indent=4, sort_keys=True))
-        return jsonString
+# class tableauData(Resource):
+#     def get(self, id):
+#         cur = mysql.connection.cursor()
+#         cur.callproc('calculateAHP1', (id))
+#         dataAhp = cur.fetchall()
+#         data = []
+#         for f in dataAhp:
+#             dat = {
+#                 'name' : f[1],
+#                 'jumlahAtt1' : f[2],
+#                 'jumlahAtt2' : f[3],
+#                 'jumlahAtt3' : f[4],
+#                 'rataAtt1' : f[5],
+#                 'rataAtt2' : f[6],
+#                 'rataAtt3' : f[7],
+#                 'matrix1' : f[8],
+#                 'matrix2' : f[9],
+#                 'matrix3' : f[10]
+#             }
+#             data.append(dat)
+#         jsonString = json.loads(json.dumps(data, indent=4, sort_keys=True))
+#         return jsonString
 
-class linkspreadsheet(Resource):
-    def post(self):
-        args = link_post_args.parse_args()
-        return args['link']
+# class linkspreadsheet(Resource):
+#     def post(self):
+#         args = link_post_args.parse_args()
+#         return args['link']
 
-api.add_resource(tableauData, "/tableauData/<string:id>")
-api.add_resource(linkspreadsheet, "/sendlink")
+# api.add_resource(tableauData, "/tableauData/<string:id>")
+# api.add_resource(linkspreadsheet, "/sendlink")
 
 # Session config
 app.secret_key = os.getenv("APP_SECRET_KEY")
@@ -78,70 +78,101 @@ app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
 
 # oAuth Setup
-oauth = OAuth(app)
-google = oauth.register(
-    name='google',
-    client_id=os.getenv("GOOGLE_CLIENT_ID"),
-    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    # userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
-    client_kwargs={'scope': 'https://www.googleapis.com/auth/drive.file'},
-)
+# oauth = OAuth(app)
+# google = oauth.register(
+#     name='google',
+#     client_id=os.getenv("GOOGLE_CLIENT_ID"),
+#     client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+#     access_token_url='https://accounts.google.com/o/oauth2/token',
+#     access_token_params=None,
+#     authorize_url='https://accounts.google.com/o/oauth2/auth',
+#     authorize_params=None,
+#     api_base_url='https://www.googleapis.com/oauth2/v1/',
+#     # userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+#     client_kwargs={'scope': 'https://www.googleapis.com/auth/drive.file'},
+# )
 
-@app.route('/')
-@login_required
+@app.route('/sendlink', methods=['POST'])
+def linkspreadsheet():
+    args = link_post_args.parse_args()
+    return jsonify(args['link'])
+
+@app.route('/tableauData/<string:id>')
+def data(id):
+    cur = mysql.connection.cursor()
+    cur.callproc('calculateAHP1', (id))
+    dataAhp = cur.fetchall()
+    data = []
+    for f in dataAhp:
+        dat = {
+            'name' : f[1],
+            'jumlahAtt1' : f[2],
+            'jumlahAtt2' : f[3],
+            'jumlahAtt3' : f[4],
+            'rataAtt1' : f[5],
+            'rataAtt2' : f[6],
+            'rataAtt3' : f[7],
+            'matrix1' : f[8],
+            'matrix2' : f[9],
+            'matrix3' : f[10]
+        }
+        data.append(dat)
+    jsonString = json.loads(json.dumps(data, indent=4, sort_keys=True))
+    return jsonify(jsonString)
+
+@app.route("/")
 def hello_world():
-    email = dict(session)['profile']['email']
-    return f'Hello, you are logged in as {email}!'
+    return "<p>Hello, World!</p>"
+# @app.route('/')
+# @login_required
+# def hello_world():
+#     email = dict(session)['profile']['email']
+#     return f'Hello, you are logged in as {email}!'
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():    
-    r = requests.get("http://localhost:8000/login")
-    print(r)
-    google = oauth.create_client('google')  # create the google oauth client
-    redirect_uri = url_for('authorize', _external=True)
-    return google.authorize_redirect(redirect_uri)
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():    
+#     r = requests.get("http://localhost:8000/login")
+#     print(r)
+#     google = oauth.create_client('google')  # create the google oauth client
+#     redirect_uri = url_for('authorize', _external=True)
+#     return google.authorize_redirect(redirect_uri)
 
-@app.route('/spreadsheet', methods=['GET', 'POST'])
-def spreedsheet():
-    if request.method == 'GET':
-        return make_response('failure')
-    if request.method == 'POST':
+# @app.route('/spreadsheet', methods=['GET', 'POST'])
+# def spreedsheet():
+#     if requests.method == 'GET':
+#         return make_response('failure')
+#     if requests.method == 'POST':
 
-        create_row_data = {'message': "Test"}
+#         create_row_data = {'message': "Test"}
         
-        response = requests.post(
-            url, data=json.dumps(create_row_data),
-            headers={'Content-Type': 'application/json'}
-        )
-        return response.content
+#         response = requests.post(
+#             url, data=json.dumps(create_row_data),
+#             headers={'Content-Type': 'application/json'}
+#         )
+#         return response.content
 
-@app.route('/authorize')
-def authorize():
-    # idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
-    # userid = idinfo['sub']
-    google = oauth.create_client('google')  # create the google oauth client
-    token = google.authorize_access_token()  # Access token from google (needed to get user info)
-    resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
-    user_info = resp.json()
-    user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
-    # Here you use the profile/user data that you got and query your database find/register the user
-    # and set ur own data in the session not the profile from google
-    session['profile'] = user_info
-    session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
-    print()
-    return redirect('http://localhost:8000/dashboard')
+# @app.route('/authorize')
+# def authorize():
+#     # idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+#     # userid = idinfo['sub']
+#     google = oauth.create_client('google')  # create the google oauth client
+#     token = google.authorize_access_token()  # Access token from google (needed to get user info)
+#     resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
+#     user_info = resp.json()
+#     user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
+#     # Here you use the profile/user data that you got and query your database find/register the user
+#     # and set ur own data in the session not the profile from google
+#     session['profile'] = user_info
+#     session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
+#     print()
+#     return redirect('http://localhost:8000/dashboard')
 
 
-@app.route('/logout')
-def logout():
-    for key in list(session.keys()):
-        session.pop(key)
-    return redirect('http://localhost:8000/')
+# @app.route('/logout')
+# def logout():
+#     for key in list(session.keys()):
+#         session.pop(key)
+#     return redirect('http://localhost:8000/')
 
 
 
